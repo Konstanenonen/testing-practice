c5b98ec892bf83f9eac53b381b243a33a5ec292b
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-24 01:13:14 +0300

    Added a test for reverseString function and then implemented the function

--- main.test.js
+++ main.test.js
@@ -1,5 +1,17 @@
-import { capitalize } from "./main";
+import { capitalize, reverseString } from "./main";
 
-test("dog should change to Dog", () => {
+test("capitalize TEST 1: dog should change to Dog", () => {
   expect(capitalize("dog")).toBe("Dog");
+});
+
+test("capitalize TEST 2: konsta should change to Konsta", () => {
+  expect(capitalize("konsta")).toBe("Konsta");
+});
+
+test("reverseString TEST 1: emma should change to amme", () => {
+  expect(reverseString("emma")).toBe("amme");
+});
+
+test("reverseString TEST 1: kuumailmapallo should change to ollapamliamuuk", () => {
+  expect(reverseString("kuumailmapallo")).toBe("ollapamliamuuk");
 });
