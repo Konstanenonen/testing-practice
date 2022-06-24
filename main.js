969aabf70abf925bf2c3b5d9cf49bfcfd550beed
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-24 10:21:16 +0300

    Added a class that makes an object with basic calculation methods. Added tests to test those method.

--- main.js
+++ main.js
@@ -2,4 +2,22 @@ const capitalize = (string) => `${string[0].toUpperCase()}${string.slice(1)}`;
 
 const reverseString = (string) => string.split("").reverse().join("");
 
-export { capitalize, reverseString };
+class Calculator {
+  add(x, y) {
+    return x + y;
+  }
+
+  subtract(x, y) {
+    return x - y;
+  }
+
+  divide(x, y) {
+    return x / y;
+  }
+
+  multiply(x, y) {
+    return x * y;
+  }
+}
+
+export { capitalize, reverseString, Calculator };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./main";
+import { Calculator, capitalize, reverseString } from "./main";
 
 test("capitalize TEST 1: dog should change to Dog", () => {
   expect(capitalize("dog")).toBe("Dog");
@@ -14,4 +14,24 @@ test("reverseString TEST 1: emma should change to amme", () => {
 
 test("reverseString TEST 1: kuumailmapallo should change to ollapamliamuuk", () => {
   expect(reverseString("kuumailmapallo")).toBe("ollapamliamuuk");
+});
+
+test("add TEST 1: 3 + 7 should equal to 10", () => {
+  const calculator = new Calculator();
+  expect(calculator.add(3, 7)).toEqual(10);
+});
+
+test("substract TEST 1: 3 - 7 should equal to -4", () => {
+  const calculator = new Calculator();
+  expect(calculator.subtract(3, 7)).toEqual(-4);
+});
+
+test("divide TEST 1: 3 / 7 should equal close to 0.42857", () => {
+  const calculator = new Calculator();
+  expect(calculator.divide(3, 7)).toBeCloseTo(3 / 7);
+});
+
+test("multiply TEST 1: 3 * 7 should equal 21", () => {
+  const calculator = new Calculator();
+  expect(calculator.multiply(3, 7)).toEqual(21);
 });
